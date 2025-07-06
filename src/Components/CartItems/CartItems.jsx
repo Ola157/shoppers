aea007a4d521be3f97d6eae6725b0212ba1f96a8
Abdullahi Alao <ridwanoladokun005@gmail.com>
2025-07-06 12:25:36 -0400

    updated

--- src/Components/CartItems/CartItems.jsx
+++ src/Components/CartItems/CartItems.jsx
@@ -4,7 +4,7 @@ import { CategoryContext } from "../../Context/ShopContext";
 import "./CartItems.css";
 import cart_cross_icon from "../Assets/cart_cross_icon.png";
 const CartItems = () => {
-  const { all_product, removeFromCart, cartItems } =
+  const { all_product, removeFromCart, cartItems, getTotalCartAmount } =
     useContext(CategoryContext);
 
   return (
@@ -29,6 +29,7 @@ const CartItems = () => {
                 <button>{cartItems[item.id]}</button>
                 <p>{item.new_price * cartItems[item.id]}</p>
                 <img
+                  className="remove-button"
                   src={cart_cross_icon}
                   alt=""
                   onClick={() => removeFromCart(item.id)}
@@ -39,6 +40,35 @@ const CartItems = () => {
           );
         }
       })}
+      <div className="cartitems-down">
+        <div className="cartitems-total">
+          <h1>Cart Totals</h1>
+          <div>
+            <div className="cartitems-total-item">
+              <p>Subtotal</p>
+              <p>${getTotalCartAmount()}</p>
+            </div>
+            <hr />
+            <div className="cartitems-total-item">
+              <p>Shipping Fee</p>
+              <p>Free</p>
+            </div>
+            <hr />
+            <div className="cartitems-total-item">
+              <p>Total</p>
+              <p>${getTotalCartAmount()}</p>
+            </div>
+          </div>
+          <button>PROCEED TO CHECKOUT</button>
+        </div>
+        <div className="cartitems-promocode">
+          <p>If you have a promo code, Enter it here</p>
+          <div className="cartitems-promobox">
+            <input type="text" placeholder="promo code" />
+            <button>Submit</button>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
